1cd6e2c7cbe6d7de7fdda5c93f3271cf0fcb154f
kunal <110029363+kp2599@users.noreply.github.com>
2024-06-05 14:20:08 +0530

    Updated start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,8 +1,18 @@
+# start_container.sh
 #!/bin/bash
 set -e
 
+# Log the start of the script
+echo "Starting start_container.sh script" | tee -a /var/log/codedeploy-agent/start_container.log
+
 # Pull the Docker image from Docker Hub
-echo
+echo "Pulling the latest Docker image..." | tee -a /var/log/codedeploy-agent/start_container.log
+# Replace 'my_image' with the actual image name
+docker pull my_image:latest
 
 # Run the Docker image as a container
-echo
+echo "Starting the Docker container..." | tee -a /var/log/codedeploy-agent/start_container.log
+# Replace 'my_container' with the desired container name and 'my_image' with the actual image name
+docker run -d --name my_container my_image:latest
+
+echo "Container started." | tee -a /var/log/codedeploy-agent/start_container.log
